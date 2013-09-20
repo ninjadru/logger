d5e02ba9ae0dbc5d6c7d49cba3f866e10ba5afc7
ninjadru <evil.little.dru@gmail.com>
2013-09-20 16:06:26 -0400

    Update logger.sh
    Made output directory an environmental variable

--- logger.sh
+++ logger.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 #This script records ssh session to a text file
+#v0.2
 
 DATE=$(date +"%m%d%y-%T")
 HOST=$1
-DIR="~/logger"
-OUT="$DIR/$HOST-$DATE"
+OUT="$HOME/logger/logs/$HOST-$DATE"
 ssh $HOST |tee -a $OUT
+
